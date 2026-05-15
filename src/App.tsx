9aff1c0ac8a5eaa29ddcb327bc291b0c44c841b9
ThinkingRocks <saharshsamir10@gmail.com>
2026-05-15 13:16:54 +0530

    virtual list for pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,45 +4,28 @@ import * as pdfjsLib from 'pdfjs-dist';
 
 const PAGES_BUFFER = 2;
 
+/* @TODO: map page number to wrapper div instead of the canvas */
+/** maps the page number to the canvas element that renders the page */
+const renderedPages: Map<number, HTMLDivElement> = new Map();
 
-const renderedPages: Map<number, HTMLCanvasElement> = new Map();
 function App() {
   const [files, setFiles] = useState<FileList | null>(null);
   const [pageCount, setPageCount] = useState(0);
 
   const containerRef = useRef<HTMLDivElement>(null);
   const documentRef = useRef<pdfjsLib.PDFDocumentProxy | null>(null);
-  const observerRef = useRef<IntersectionObserver | null>(null);
   const visiblePagesRef = useRef<number>(0);
   const firstRenderedPageIdxRef = useRef<number>(1);
-  const lastRenderedPageIdxRef = useRef<number>(1);
-  /** maps the page number to the canvas element that renders the page */
+  const lastRenderedPageIdxRef = useRef<number>(0);
+  const scrollTopRef = useRef<number>(0);
+  const pageHeightRef = useRef<number>(0);
+  const currentVisiblePage = useRef<number>(1);
 
   function yieldToBrowser() {
     return new Promise((resolve) => {
       setTimeout(resolve, 0);
     })
   }
-
-  useEffect(() => {
-    observerRef.current = new IntersectionObserver((entries) => {
-
-      const doc = documentRef.current;
-      if (!doc) return;
-
-      const entry = entries[0];
-      if (entry.isIntersecting) {
-
-        firstRenderedPageIdxRef.current += PAGES_BUFFER;
-        lastRenderedPageIdxRef.current += PAGES_BUFFER;
-
-        unmountPages();
-
-        observerRef.current?.unobserve(entry.target);
-        renderNextPages(firstRenderedPageIdxRef.current, lastRenderedPageIdxRef.current);
-      }
-    })
-  }, []);
 
   useEffect(() => {
     //render pages
@@ -60,35 +43,58 @@ function App() {
       //this is where we need to calculate the total pages to render at a given moment
       const viewportHeight = window.innerHeight;
       const pageHeight = (await doc.getPage(1)).getViewport({ scale: 1 }).height;
+      pageHeightRef.current = pageHeight;
 
       const pagesVisible = Math.ceil(viewportHeight / pageHeight);
       visiblePagesRef.current = pagesVisible;
 
       lastRenderedPageIdxRef.current = Math.min(pagesVisible + PAGES_BUFFER, doc.numPages);
 
-      await renderNextPages(firstRenderedPageIdxRef.current, lastRenderedPageIdxRef.current); //initial load
+      await moveRenderWindow(firstRenderedPageIdxRef.current, lastRenderedPageIdxRef.current); //initial load
+      addScrollListener();
     }
 
     load();
 
   }, [files]);
 
-  function unmountPages() {
+  function addScrollListener() {
+    window.addEventListener('scroll', () => {
+      scrollTopRef.current = window.scrollY + 50;
+      if (pageHeightRef.current && documentRef.current) {
+        const currentPage = Math.ceil(scrollTopRef.current / pageHeightRef.current);
+        const totalpages = documentRef.current.numPages;
+        //if visible page changed
+        if (currentPage !== currentVisiblePage.current) {
+          currentVisiblePage.current = currentPage;
 
-    renderedPages.forEach((canvas, pageNumber) => {
-      if (pageNumber < firstRenderedPageIdxRef.current) {
-        console.log('removing pages, ', pageNumber);
-        canvas.remove();
-        renderedPages.delete(pageNumber);
+          firstRenderedPageIdxRef.current = Math.max(currentVisiblePage.current - PAGES_BUFFER, 1);
+          lastRenderedPageIdxRef.current = Math.min(firstRenderedPageIdxRef.current + visiblePagesRef.current + PAGES_BUFFER, totalpages);
+
+          console.log(firstRenderedPageIdxRef.current, visiblePagesRef.current, PAGES_BUFFER, totalpages, currentPage);
+          moveRenderWindow(firstRenderedPageIdxRef.current, lastRenderedPageIdxRef.current);
+        }
+
+      }
+
+    })
+  }
+
+
+  function unmountPages() {
+    renderedPages.forEach((pageWrapper, pageNumber) => {
+      if (pageWrapper.hasChildNodes()) {
+        if (pageNumber < firstRenderedPageIdxRef.current || pageNumber > lastRenderedPageIdxRef.current) {
+          const canvas = pageWrapper.children[0] as HTMLCanvasElement;
+          canvas.remove();
+        }
       }
     })
   }
 
-  //this function will be for rendering the previous pages when scrolling up
-  const renderPreviousPages = async () => { }
 
   //this function will be for rendering the next pages when scrolling down
-  const renderNextPages = async (start: number, end: number) => {
+  const moveRenderWindow = async (start: number, end: number) => {
     const doc = documentRef.current!;
     const container = containerRef.current!;
     // [start....end] window represents the range of pages to render
@@ -98,7 +104,8 @@ function App() {
     //pages and the loop counter are both 1 indexed
     //load first x pages to render
     for (let i = start; i <= end; i++) {
-      if (renderedPages.get(i)) continue;
+
+      if (renderedPages.get(i)?.hasChildNodes()) continue;
 
       const page = await doc.getPage(i);
       const canvas = document.createElement("canvas");
@@ -118,19 +125,28 @@ function App() {
       }
 
       await page.render(params).promise;
+      const pageWrapper = renderedPages.get(i);
+      const pageContainer = pageWrapper ? pageWrapper : document.createElement("div");
+
 
       canvas.setAttribute("id", `${i}`);
-      renderedPages.set(i, canvas);
 
-      container.appendChild(canvas);
+      pageContainer.setAttribute("style", `height: ${canvas.height}px; width: ${canvas.width}px`);
+      pageContainer.style.height = `${canvas.height}px`;
+      pageContainer.style.width = `${canvas.width}px`;
+      pageContainer.append(canvas);
 
-      if (i === end) {
-        observerRef.current?.observe(canvas);
+      if (pageWrapper === undefined) {
+        //add page (html wrapper with page canvas) to map
+        renderedPages.set(i, pageContainer);
+
+        container.appendChild(pageContainer);
       }
 
       await yieldToBrowser();
     }
 
+    unmountPages();
 
   }
 
@@ -141,9 +157,10 @@ function App() {
   return (
     <div>
       <p>Upload a pdf</p>
-      <input type="file" onChange={(e) => uploadFile(e)} />
+      <input className="border border-white" type="file" onChange={(e) => uploadFile(e)} />
       <p>pages: {pageCount}</p>
-      <div ref={containerRef} />
+      <p>scroll top: {scrollTopRef.current}</p>
+      <div className="flex flex-col items-center" ref={containerRef} />
     </div>
   )
 }
